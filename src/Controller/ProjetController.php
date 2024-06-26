3a554ea198400fc6e308b6711a7896f09e0d405c
Pierre Nogaro <pierre.nogaro@gmail.com>
2024-06-26 12:12:37 +0200

    profil, compétences, mes services & début mes projets

--- src/Controller/ProjetController.php
+++ src/Controller/ProjetController.php
@@ -2,20 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Projet;
 use App\Repository\ProjetRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController extends AbstractController
+class ProjetController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/projets', name: 'app_projets')]
     public function index(ProjetRepository $projetRepository): Response
     {
         $projets = $projetRepository->findAll();
 
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('projet/index.html.twig', [
+
             'projets' => $projets,
         ]);
     }
